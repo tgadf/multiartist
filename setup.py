c786ae3b417d64a06ac9390d31f8f8074f581357
tgadf <tgadfort@gmail.com>
2020-01-21 19:13:25 -0600

    1st one

---
+++ setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+import setuptools
+
+setup(
+  name = 'multiartist',
+  py_modules = ['multiArtist'],
+  version = '0.0.1',
+  description = 'A Python Wrapper To Parse Music Chart Data',
+  long_description = open('README.md').read(),
+  author = 'Thomas Gadfort',
+  author_email = 'tgadfort@gmail.com',
+  license = "MIT",
+  url = 'https://github.com/tgadf/multiartist',
+  keywords = ['music'],
+  classifiers = [
+    'Development Status :: 3',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: Apache Software License',
+    'Programming Language :: Python',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    'Topic :: Utilities'
+  ],
+  install_requires=['utils==0.0.1', 'discogs==0.0.1'],
+  dependency_links=['git+ssh://git@github.com/tgadf/utils.git#egg=utils-0.0.1',
+                    'git+ssh://git@github.com/tgadf/discogs.git#egg=discogs-0.0.1']
+)
+ 
